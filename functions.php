c8e2fcf5c0c86427060580653e180408d436c334
rcopestake <richard@redink.co.uk>
2021-03-23 14:18:54 +0000

    design and assets pages started

--- functions.php
+++ functions.php
@@ -61,6 +61,7 @@ add_action( 'init', 'wpb_custom_footer_menu' );
 
 function altea_theme_support(){
   add_theme_support('post-thumbnails');
+
 }
 add_action ('after_setup_theme', 'altea_theme_support');
 ?>
