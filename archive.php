f02e1b7fc08f732c0661b831027d2bfe52adf9f0
rcopestake <richard@redink.co.uk>
2021-04-28 15:47:29 +0100

    news posts page in

---
+++ archive.php
@@ -0,0 +1,21 @@
+<?php /* Template Name: Archive page Custom */ ?>
+<?php get_header();?>
+
+<?php
+if( have_posts() ) :
+    while ( have_posts() ) :
+        the_post(); ?>
+
+<section>
+
+    <div class="container">
+
+        <?php the_content(); ?>
+
+
+
+        <?php endwhile; endif; ?>
+    </div>
+</section>
+
+<?php get_footer();?>
--- home.php
+++ home.php
@@ -1,25 +1,40 @@
 <?php
 get_header();
 ?>
-
+<?php
+echo do_shortcode('[smartslider3 slider="13"]');
+?>
+<div class="hero-wrapper">
+    <a href="#main-content"><i class="fas fa-chevron-down fa-5x"></i></a>
+</div>
 <article class="container">
+    <div class="posts-grid" id="main-content">
 
-    <?php
+        <?php
 
 if( have_posts() ){
 
     while( have_posts() ){
+?>
+        <div class="grid-item">
+            <?php        the_post(); ?>
 
-        the_post(); ?>
-    <a href="<?php the_permalink(); ?>">
-        <?php the_title(); ?>
-    </a>
-    <?php  the_content(); 
-    }
+
+            <?php  if ( has_post_thumbnail() ) {
+    the_post_thumbnail();
+}   ?>
+
+            <h2><a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a></h2>
+
+            <?php the_excerpt(); ?>
+        </div>
+        <?php
+}  
 }
 
 ?>
 
+    </div>
 
 
 </article>
