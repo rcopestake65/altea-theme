497a7730a2d770c4d877b72d53c48c406674a39c
rcopestake <richard@redink.co.uk>
2021-05-12 17:10:42 +0100

    small amends pre-live

---
+++ 404.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+?>
+<div class="container">
+    <div class="error-container">
+        <div class="error-content">
+            <h2>Oops, looks the page you requested can't be found</h2>
+            <h3>Please use the navigation above to go back to the site</h3>
+        </div>
+
+    </div>
+</div>
+
+
+<?php
+get_footer();
